197962e687ea89e52c9696b3893e69f2ac5476ee
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2019-07-23 14:37:06 +0100

    Added restart request parsing logic

--- handlers.go
+++ handlers.go
@@ -2,15 +2,60 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
-func healthz(w http.ResponseWriter, r *http.Request) {
-	writeJSONResponse(w, http.StatusOK, "message", "OK 👍🏼")
+// When a restart reason is not specified we assume user wants to restart
+// an application because its data changed.
+const defaultReason = "data-changed"
+
+// RestartRequest contains the host to restart and the reason for restart.
+type RestartRequest struct {
+	Host   string `json:"host"`
+	Reason string `json:"reason"`
 }
 
+// POST /restart
+// {
+//   "host": "example.com",
+//   "reason": "data-updated"
+// }
 func restart(w http.ResponseWriter, r *http.Request) {
+	req, err := getRestartRequest(r.Body)
+	if err != nil {
+		errMsg := fmt.Sprintf("invalid restart request: %s", err)
+		log.Print(errMsg)
+		writeJSONResponse(w, http.StatusBadRequest, "error", errMsg)
+		return
+	}
+
+	logger.Printf("%s: restart request received. Reason: '%s'", req.Host, req.Reason)
+
 	writeJSONResponse(w, http.StatusNotImplemented, "error", "TODO ⚠️")
+}
+
+func getRestartRequest(r io.Reader) (RestartRequest, error) {
+	req := RestartRequest{}
+	err := json.NewDecoder(r).Decode(&req)
+	if err != nil {
+		return req, fmt.Errorf("failed to parse restart request: %s", err)
+	}
+
+	if req.Host == "" {
+		return req, fmt.Errorf("'host' can't be blank")
+	}
+	if req.Reason == "" {
+		req.Reason = defaultReason
+	}
+
+	return req, nil
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	writeJSONResponse(w, http.StatusOK, "message", "OK 👍🏼")
 }
 
 func writeJSONResponse(w http.ResponseWriter, status int, messageKey string, message string) {
